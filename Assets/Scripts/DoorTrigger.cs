e8b28130438e1d5a7e25046c2f000506a7a497ac
sabukar <suada.abukar@students.unibe.ch>
2021-04-06 18:49:37 +0200

    Added soundtrack and door opening sound.

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -6,11 +6,14 @@ public class DoorTrigger : MonoBehaviour
 {
 
 	[SerializeField] private Animator anim;
+	private SFXManager sfxManager;
 
     private void OnTriggerEnter2D(Collider2D other){
+    	sfxManager = FindObjectOfType<SFXManager>();
     	//player on trigger
     	if(other.tag == "Player" || other.tag == "Trigger"){
         		anim.SetBool("isOpen", true);
+        		sfxManager.doorOpen.Play();
         		//Debug.Log("Collider enterd: " + other.tag);
 	    }
     }
