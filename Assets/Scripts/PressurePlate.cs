7d93765a9d15620f1554055126037aaae8fa224f
sabukar <suada.abukar@students.unibe.ch>
2021-04-14 19:23:36 +0200

    Update level 2

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -10,17 +10,29 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour
 {
 	[SerializeField] private Animator anim;
+	[SerializeField] private int limit; // number necessary triggers for event to happen
+	private SFXManager sfxManager;
+	public static int count = 0;
+
 
     private void OnTriggerEnter2D(Collider2D other){
-    	if(other.tag == "Trigger"){
+    	sfxManager = FindObjectOfType<SFXManager>();
+    	count++;
+    	if(other.tag == "Trigger" && count == limit){
+        		sfxManager.doorOpen.Play(); //Play door opening sound
         		anim.SetBool("isOpen", true);
+
 	    }
+	    Debug.Log("Enter: " + count);
     }
 
     private void OnTriggerExit2D(Collider2D other){
-    	if(other.tag == "Trigger"){
+    	count--;
+    	if(other.tag == "Trigger" && count == limit){
         		anim.SetBool("isOpen", false);
 	    }
+	    Debug.Log("Exit: " + count);
 	}
+
 
 }
