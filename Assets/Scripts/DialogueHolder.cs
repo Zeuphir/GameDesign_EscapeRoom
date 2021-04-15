20184aa9206b72ade05df3a991b03cb6cd75a2d2
Zeuphir <38885876+Zeuphir@users.noreply.github.com>
2021-04-15 11:18:39 +0200

    nre try

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour
+{
+
+    public string dialogue;
+    private DialogueManager dMan;
+    public string[] dialogueLines;
+
+    private bool inside;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dMan = FindObjectOfType<DialogueManager>();
+        inside = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inside && Input.GetKeyDown(KeyCode.F))
+        {
+
+            if (!dMan.dialogActive)
+            {
+                dMan.dialogueLines = dialogueLines;
+                dMan.currentLine = 0;
+                dMan.ShowDialogue();
+            }
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inside = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inside = false;
+        }
+    }
+}
