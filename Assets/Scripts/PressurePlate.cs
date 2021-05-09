a0dcce2a1f125caed3f90847eb1ccee94d6552c4
sabukar <suada.abukar@students.unibe.ch>
2021-05-09 18:32:55 +0200

    Egyptian god puzzle

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 /** 
  * Generic pressure plate trigger script.
- * For use: add animator in the Unity UI and 
- * make sure that the animator has a isOpen parameter.  
+ * For use: set object name(s) that should trigger pressre plate,
+ * add animator in the Unity UI and make sure that the animator 
+ * has a isOpen parameter.  
  */
 public class PressurePlate : MonoBehaviour
 {
+	public string[] validTrigger; //List of objects that can trigger pressure plate
 	[SerializeField] private Animator anim;
 	[SerializeField] private int limit; // number necessary triggers for event to happen
 	private SFXManager sfxManager;
@@ -17,8 +19,11 @@ public class PressurePlate : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other){
     	sfxManager = FindObjectOfType<SFXManager>();
-    	count++;
-    	if(other.tag == "Trigger" && count == limit){
+    	if(Contains(validTrigger, other.name)){
+    		count++;	
+    	}
+    	
+    	if(Contains(validTrigger, other.name) && count == limit){
         		sfxManager.doorOpen.Play(); //Play door opening sound
         		anim.SetBool("isOpen", true);
 
@@ -26,13 +31,20 @@ public class PressurePlate : MonoBehaviour
 	    Debug.Log("Enter: " + count);
     }
 
-    private void OnTriggerExit2D(Collider2D other){
-    	count--;
-    	if(other.tag == "Trigger" && count == limit){
+    private void OnTriggerExit2D(Collider2D other){    	
+    	if(Contains(validTrigger, other.name)){
+    		count--;	
+    	}
+    	if(Contains(validTrigger, other.name) && count != limit){
         		anim.SetBool("isOpen", false);
 	    }
 	    Debug.Log("Exit: " + count);
 	}
 
+    public static bool Contains<T>(T[] array, T item)
+    {
+        List<T> list = new List<T>(array);
+        return list.Contains(item);
+    }
 
 }
