8cec3bb3515b42ff966ebb3036ec63121a72f029
sabukar <suada.abukar@students.unibe.ch>
2021-06-13 17:36:28 +0200

    New Level2 layout.

---
+++ Assets/SuperTiled2Unity/Import/Egyptian-wcat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="Egyptian-wcat" tilewidth="16" tileheight="16" tilecount="50" columns="10">
+ <image source="../../Art/Egypt-Tilesets-cat.png" width="160" height="80"/>
+</tileset>
